39e1e92adecde09be03c069c0a03c22e7c296a64
raihan-muhammad <sukasukangoding@gmail.com>
2022-10-14 08:45:31 +0700

    init file

---
+++ models/car.js
@@ -0,0 +1,26 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Car extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Car.init({
+    name: DataTypes.STRING,
+    size: DataTypes.STRING,
+    price: DataTypes.INTEGER,
+    photo: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'Car',
+  });
+  return Car;
+};
---
+++ seeders/20221013070344-add-cars.js
@@ -0,0 +1,32 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+
+     await queryInterface.bulkInsert('Cars', [{
+        name: 'Avanza',
+        size: "small",
+        price: 1000,
+        photo: "photo.png"
+      }], {});
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  }
+};
